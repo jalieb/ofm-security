9de927a468263f995ca9d78d6a9ec86cddb13cdf
Julius Liebscher <julius.liebscher@gmail.com>
2025-08-02 13:51:33 +0200

    Initial commit.

---
+++ OfmSecurityBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ofm\Security;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class OfmSecurityBundle extends AbstractBundle
+{
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import(__DIR__ . '/config/services.xml');
+    }
+}
